419bda619dcc623c554dd27292ada92aacce3c11
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-06-17 13:50:07 +0200

    Added ModAPI project and Harmony dll. Reference Harmony and Assembly-CSharp.dll from game directory specified in DevVars.targets file.

---
+++ GOIModdingAPI/ModAPI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Harmony;
+
+namespace ModAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var harmony = HarmonyInstance.Create("com.goimodapi");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
