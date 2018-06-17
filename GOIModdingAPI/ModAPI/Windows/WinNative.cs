f4b10f06c0a927b3dbdfdbef1c197d8b016753b9
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-06-17 21:15:54 +0200

    Added WinNative class

---
+++ GOIModdingAPI/ModAPI/Windows/WinNative.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ModAPI.Windows
+{
+    internal static class WinNative
+    {
+        [DllImport("kernel32.dll",
+            EntryPoint = "AllocConsole",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern int AllocConsole();
+
+        [DllImport("kernel32.dll",
+            EntryPoint = "AllocConsole",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern int FreeConsole();
+    }
+}
