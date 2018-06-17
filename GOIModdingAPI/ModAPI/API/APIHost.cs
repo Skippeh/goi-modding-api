0b4ab4c9a12168673dceadaf9aba738ec8e0b1f2
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-06-18 00:19:42 +0200

    Added static api host class, responsible for overall mod api state/management.

---
+++ GOIModdingAPI/ModAPI/API/APIHost.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using ModAPI.Logging;
+using ModAPI.Plugins;
+using ModAPI.Types;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ModAPI.API
+{
+    public static class APIHost
+    {
+        public static Logging.Logger Logger { get; private set; }
+        public static PluginManager Plugins { get; private set; }
+
+        internal static void Initialize()
+        {
+            Logger = new Logging.Logger(new LogOptions
+            {
+                LogDirectory = "Logs",
+                LogToConsole = true
+            });
+            
+            Plugins = new PluginManager();
+            Plugins.StartListening();
+            
+            Application.logMessageReceived += OnLogMessageReceived;
+            SceneManager.activeSceneChanged += OnNewScene;
+        }
+        
+        private static void OnLogMessageReceived(string condition, string stacktrace, LogType type)
+        {
+            string logString = condition + (stacktrace != null ? "\n" + stacktrace : "");
+            logString = logString.Trim();
+            
+            switch (type)
+            {
+                case LogType.Log:
+                    Logger.LogInfo(logString);
+                    break;
+                case LogType.Warning:
+                    Logger.LogWarning(logString);
+                    break;
+                case LogType.Error:
+                case LogType.Assert:
+                case LogType.Exception:
+                    Logger.LogError(logString);
+                    break;
+            }
+        }
+        
+        private static void OnNewScene(Scene oldScene, Scene newScene)
+        {
+            //Interface.CallHook("OnSceneChanged", GetSceneType(newScene), newScene);
+        }
+
+        private static SceneType GetSceneType(Scene newScene)
+        {
+            switch (newScene.name)
+            {
+                case "Loader":
+                    return SceneType.Menu;
+                case "Mian":
+                    return SceneType.Game;
+                case "Credits":
+                    return SceneType.Credits;
+                default:
+                    Logger.LogError($"Unknown scene loaded: \"{newScene.name}\"");
+                    return SceneType.Invalid;
+            }
+        }
+    }
+}
