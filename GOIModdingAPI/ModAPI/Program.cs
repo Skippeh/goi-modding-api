8fafd3072820a926b67872be91f389bef9a9922d
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-06-18 00:21:33 +0200

    Initialize console. AllocConsole if on windows

--- GOIModdingAPI/ModAPI/Program.cs
+++ GOIModdingAPI/ModAPI/Program.cs
@@ -1,14 +1,35 @@
-﻿using System.Reflection;
+﻿using System;
+using System.IO;
+using System.Reflection;
 using Harmony;
+using ModAPI.API;
+using ModAPI.Windows;
 
 namespace ModAPI
 {
     public class Program
     {
+        private static StreamWriter consoleWriter;
+        
         public static void Main()
         {
+            InitializeConsole();
+            
             var harmony = HarmonyInstance.Create("com.goimodapi");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            APIHost.Initialize();
+        }
+
+        private static void InitializeConsole()
+        {
+            if (Environment.OSVersion.Platform != PlatformID.MacOSX && Environment.OSVersion.Platform != PlatformID.Unix)
+            {
+                WinNative.AllocConsole();
+            }
+            
+            consoleWriter = new StreamWriter(Console.OpenStandardOutput()) {AutoFlush = true};
+            Console.SetOut(consoleWriter);
         }
     }
 }
