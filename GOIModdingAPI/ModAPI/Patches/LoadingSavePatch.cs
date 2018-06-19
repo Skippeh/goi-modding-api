4471281e67bb0562fe630ad7aed44b3f88769447
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-06-19 23:51:14 +0200

    Added runtime hook for loading save

---
+++ GOIModdingAPI/ModAPI/Patches/LoadingSavePatch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Harmony;
+using ModAPI.API;
+
+namespace ModAPI.Patches
+{
+    [HarmonyPatch(typeof(Saviour))]
+    [HarmonyPatch("Load")]
+    [HarmonyPatch(new[] {typeof(SaveState)})]
+    internal static class LoadingSavePatch
+    {
+        private static void Prefix(SaveState saveState)
+        {
+            APIHost.Events.OnLoadingSave(saveState);
+        }
+    }
+}
