633df8a7e563a64fb5aca74a55f648fef19dfec2
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-06-22 13:48:20 +0200

    Add plugin attribute to sample plugin

--- GOIModdingAPI/SamplePlugin/SamplePlugin.cs
+++ GOIModdingAPI/SamplePlugin/SamplePlugin.cs
@@ -3,6 +3,7 @@ using ModAPI.Plugins;
 
 namespace SamplePlugin
 {
+    [Plugin("Sample Plugin", "Skippy", ShortDescription = "A sample plugin to showcase functionality and usage.")]
     public class SamplePlugin : Plugin
     {
         protected override void Initialize()
