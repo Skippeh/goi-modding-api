a3e18b5f614f8b4962f2af1266638a689fb9302f
Jonathan Lindahl <jonathanjl99@gmail.com>
2018-06-22 03:54:49 +0200

    Added sample plugin project

---
+++ GOIModdingAPI/SamplePlugin/SamplePlugin.cs
@@ -0,0 +1,13 @@
+﻿using ModAPI.API;
+using ModAPI.Plugins;
+
+namespace SamplePlugin
+{
+    public class SamplePlugin : Plugin
+    {
+        protected override void Initialize()
+        {
+            APIHost.Logger.LogDebug("SamplePlugin initializing");
+        }
+    }
+}
